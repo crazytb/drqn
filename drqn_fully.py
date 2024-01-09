0533c87941405e10d76c2d16257e4ca10fc2d3ff
Taewon Song <crazytb@gmail.com>
2024-01-09 11:11:08 +0900

    Revice main

--- drqn_fully.py
+++ drqn_fully.py
@@ -13,6 +13,13 @@ import torch.optim as optim
 
 from torch.utils.tensorboard import SummaryWriter
 
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+elif torch.backends.mps.is_available():
+    device = torch.device("mps") if torch.backends.mps.is_available() else "cpu"
+else:
+    device = torch.device("cpu")
+
 
 # Q_network
 class Q_net(nn.Module):

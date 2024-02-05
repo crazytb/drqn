b6f05fb149a03260f41caee8f9455796107e429a
Taewon Song <crazytb@gmail.com>
2024-02-05 18:50:49 +0900

    To do: make env and agent based on DRQN.py.

--- main_drqn_fully.py
+++ main_drqn_fully.py
@@ -1,11 +1,7 @@
 # https://github.com/keep9oing/DRQN-Pytorch-CartPole-v1
 
-import sys
-from typing import Dict, List, Tuple
 
-import gymnasium as gym
-from gymnasium.wrappers import FlattenObservation
-import collections
+
 import random
 import numpy as np
 import networkx as nx
@@ -17,7 +13,7 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils.tensorboard import SummaryWriter
 
-from environment import PNDEnv
+import environment as env
 
 
 if torch.cuda.is_available():
@@ -29,251 +25,12 @@ else:
 
 print(device)
 
-def make_adjacency_matrix(n: int, density: float) -> np.ndarray:
-    """Make adjacency matrix of a clique network.
-    
-    Args:
-        n (int): Number of nodes.
-        density (float): Density of the clique network.
-    
-    Returns:
-        np.ndarray: Adjacency matrix.
-    """
-    if density < 0 or density > 1:
-        raise ValueError("Density must be between 0 and 1.")
-    
-    n_edges = int(n * (n - 1) / 2 * density)
-    adjacency_matrix = np.zeros((n, n))
-    
-    for i in range(1, n):
-        adjacency_matrix[i-1, i] = 1
-        adjacency_matrix[i, i-1] = 1
-        n_edges -= 1
-    
-    # If the density of the current adjacency matrix is over density, return it.
-    if n_edges <= 0:
-        return adjacency_matrix
-    else:
-        arr = [1]*n_edges + [0]*((n-1)*(n-2)//2 - n_edges)
-        np.random.shuffle(arr)
-        for i in range(0, n):
-            for j in range(i+2, n):
-                adjacency_matrix[i, j] = arr.pop()
-                adjacency_matrix[j, i] = adjacency_matrix[i, j]
-    return adjacency_matrix
 
-def show_graph_with_labels(adjacency_matrix):
-    rows, cols = np.where(adjacency_matrix == 1)
-    edges = zip(rows.tolist(), cols.tolist())
-    G = nx.Graph()
-    G.add_edges_from(edges)
-    pos = nx.kamada_kawai_layout(G)
-    nx.draw_networkx(G, pos=pos, with_labels=True)
-    plt.show()
-    
-
-adj = make_adjacency_matrix(10, 0.5)
-show_graph_with_labels(adj)
+adj = env.make_adjacency_matrix(10, model="dumbbell")
+env.show_graph_with_labels(adj)
 
 
 # Q_network
-class Q_net(nn.Module):
-    def __init__(self, state_space=None,
-                 action_space=None):
-        super(Q_net, self).__init__()
-        # space size check
-        assert state_space is not None, "None state_space input: state_space should be selected."
-        assert action_space is not None, "None action_space input: action_space should be selected."
-
-        self.hidden_space = 64
-        self.state_space = state_space
-        self.action_space = action_space
-
-        self.Linear1 = nn.Linear(self.state_space, self.hidden_space)
-        self.lstm    = nn.LSTM(self.hidden_space, self.hidden_space, batch_first=True)
-        self.Linear2 = nn.Linear(self.hidden_space, self.action_space)
-
-    def forward(self, x, h, c):
-        x = F.relu(self.Linear1(x))
-        x, (new_h, new_c) = self.lstm(x,(h,c))
-        x = self.Linear2(x)
-        return x, new_h, new_c
-
-    def sample_action(self, obs, h,c, epsilon):
-        output = self.forward(obs, h,c)
-
-        if random.random() < epsilon:
-            return random.randint(0,1), output[1], output[2]
-        else:
-            return output[0].argmax().item(), output[1] , output[2]
-    
-    def init_hidden_state(self, batch_size, training=None):
-        assert training is not None, "training step parameter should be dtermined"
-        if training is True:
-            return torch.zeros([1, batch_size, self.hidden_space]), torch.zeros([1, batch_size, self.hidden_space])
-        else:
-            return torch.zeros([1, 1, self.hidden_space]), torch.zeros([1, 1, self.hidden_space])
-
-class EpisodeMemory():
-    """Episode memory for recurrent agent"""
-    def __init__(self, random_update=False, 
-                       max_epi_num=100, max_epi_len=500,
-                       batch_size=1,
-                       lookup_step=None):
-        self.random_update = random_update # if False, sequential update
-        self.max_epi_num = max_epi_num
-        self.max_epi_len = max_epi_len
-        self.batch_size = batch_size
-        self.lookup_step = lookup_step
-
-        if (random_update is False) and (self.batch_size > 1):
-            sys.exit('It is recommend to use 1 batch for sequential update, if you want, erase this code block and modify code')
-
-        self.memory = collections.deque(maxlen=self.max_epi_num)
-
-    def put(self, episode):
-        self.memory.append(episode)
-
-    def sample(self):
-        sampled_buffer = []
-
-        ##################### RANDOM UPDATE ############################
-        if self.random_update: # Random update
-            sampled_episodes = random.sample(self.memory, self.batch_size)
-            
-            check_flag = True # check if every sample data to train is larger than batch size
-            min_step = self.max_epi_len
-
-            for episode in sampled_episodes:
-                min_step = min(min_step, len(episode)) # get minimum step from sampled episodes
-
-            for episode in sampled_episodes:
-                if min_step > self.lookup_step: # sample buffer with lookup_step size
-                    idx = np.random.randint(0, len(episode)-self.lookup_step+1)
-                    sample = episode.sample(random_update=self.random_update, lookup_step=self.lookup_step, idx=idx)
-                    sampled_buffer.append(sample)
-                else:
-                    idx = np.random.randint(0, len(episode)-min_step+1) # sample buffer with minstep size
-                    sample = episode.sample(random_update=self.random_update, lookup_step=min_step, idx=idx)
-                    sampled_buffer.append(sample)
-
-        ##################### SEQUENTIAL UPDATE ############################           
-        else: # Sequential update
-            idx = np.random.randint(0, len(self.memory))
-            sampled_buffer.append(self.memory[idx].sample(random_update=self.random_update))
-
-        return sampled_buffer, len(sampled_buffer[0]['obs']) # buffers, sequence_length
-
-    def __len__(self):
-        return len(self.memory)
-
-
-class EpisodeBuffer:
-    """A simple numpy replay buffer."""
-
-    def __init__(self):
-        self.obs = []
-        self.action = []
-        self.reward = []
-        self.next_obs = []
-        self.done = []
-
-    def put(self, transition):
-        self.obs.append(transition[0])
-        self.action.append(transition[1])
-        self.reward.append(transition[2])
-        self.next_obs.append(transition[3])
-        self.done.append(transition[4])
-
-    def sample(self, random_update=False, lookup_step=None, idx=None) -> Dict[str, np.ndarray]:
-        obs = np.array(self.obs)
-        action = np.array(self.action)
-        reward = np.array(self.reward)
-        next_obs = np.array(self.next_obs)
-        done = np.array(self.done)
-
-        if random_update is True:
-            obs = obs[idx:idx+lookup_step]
-            action = action[idx:idx+lookup_step]
-            reward = reward[idx:idx+lookup_step]
-            next_obs = next_obs[idx:idx+lookup_step]
-            done = done[idx:idx+lookup_step]
-
-        return dict(obs=obs,
-                    acts=action,
-                    rews=reward,
-                    next_obs=next_obs,
-                    done=done)
-
-    def __len__(self) -> int:
-        return len(self.obs)
-
-
-def train(q_net=None, target_q_net=None, episode_memory=None,
-          device=None, 
-          optimizer = None,
-          batch_size=1,
-          learning_rate=1e-3,
-          gamma=0.99):
-
-    assert device is not None, "None Device input: device should be selected."
-
-    # Get batch from replay buffer
-    samples, seq_len = episode_memory.sample()
-
-    observations = []
-    actions = []
-    rewards = []
-    next_observations = []
-    dones = []
-
-    for i in range(batch_size):
-        observations.append(samples[i]["obs"])
-        actions.append(samples[i]["acts"])
-        rewards.append(samples[i]["rews"])
-        next_observations.append(samples[i]["next_obs"])
-        dones.append(samples[i]["done"])
-
-    observations = np.array(observations)
-    actions = np.array(actions)
-    rewards = np.array(rewards)
-    next_observations = np.array(next_observations)
-    dones = np.array(dones)
-
-    observations = torch.FloatTensor(observations.reshape(batch_size,seq_len,-1)).to(device)
-    actions = torch.LongTensor(actions.reshape(batch_size,seq_len,-1)).to(device)
-    rewards = torch.FloatTensor(rewards.reshape(batch_size,seq_len,-1)).to(device)
-    next_observations = torch.FloatTensor(next_observations.reshape(batch_size,seq_len,-1)).to(device)
-    dones = torch.FloatTensor(dones.reshape(batch_size,seq_len,-1)).to(device)
-
-    h_target, c_target = target_q_net.init_hidden_state(batch_size=batch_size, training=True)
-
-    q_target, _, _ = target_q_net(next_observations, h_target.to(device), c_target.to(device))
-
-    q_target_max = q_target.max(2)[0].view(batch_size,seq_len,-1).detach()
-    targets = rewards + gamma*q_target_max*dones
-
-
-    h, c = q_net.init_hidden_state(batch_size=batch_size, training=True)
-    q_out, _, _ = q_net(observations, h.to(device), c.to(device))
-    q_a = q_out.gather(2, actions)
-
-    # Multiply Importance Sampling weights to loss        
-    loss = F.smooth_l1_loss(q_a, targets)
-    
-    # Update Network
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
-
-def seed_torch(seed):
-        torch.manual_seed(seed)
-        if torch.backends.cudnn.enabled:
-            torch.backends.cudnn.benchmark = False
-            torch.backends.cudnn.deterministic = True
-
-def save_model(model, path='default.pth'):
-        torch.save(model.state_dict(), path)
 
 if __name__ == "__main__":
 
@@ -285,7 +42,7 @@ if __name__ == "__main__":
 
     # Set gym environment
     # env = gym.make("CartPole-v1")
-    env = PNDEnv()
+    cusenv = env.PNDEnv()
     # env = FlattenObservation(env)
 
     if torch.cuda.is_available():
@@ -325,9 +82,9 @@ if __name__ == "__main__":
 
     # Create Q functions
     Q = Q_net(state_space=5, # env.observation_space.shape[0]
-              action_space=env.action_space.n).to(device)
+              action_space=cusenv.action_space.n).to(device)
     Q_target = Q_net(state_space=5, 
-                     action_space=env.action_space.n).to(device)
+                     action_space=cusenv.action_space.n).to(device)
 
     Q_target.load_state_dict(Q.state_dict())
 
@@ -345,7 +102,7 @@ if __name__ == "__main__":
 
     # Train
     for i in range(episodes):
-        s, _ = env.reset(seed=seed)
+        s, _ = cusenv.reset(seed=seed)
         obs = s
         done = False
         
@@ -361,7 +118,7 @@ if __name__ == "__main__":
                                       epsilon)
 
             # Do action
-            s_prime, r, done, _, _ = env.step(a)
+            s_prime, r, done, _, _ = cusenv.step(a)
             obs_prime = s_prime
 
             # make data
@@ -403,4 +160,4 @@ if __name__ == "__main__":
         score = 0
         
     writer.close()
-    env.close()
+    cusenv.close()
